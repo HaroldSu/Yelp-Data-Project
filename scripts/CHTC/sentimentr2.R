e54a3dac63c1f948fb59a30dff0b49375bccc892
Augustine Runshi Tang <42051642+TongRunshi@users.noreply.github.com>
2020-11-23 10:50:27 -0600

    Codes used for sentiment and generating .csv files
    2 versions and we used the first(without 2)

--- scripts/CHTC/sentimentr2.R
+++ scripts/CHTC/sentimentr2.R
@@ -16,12 +16,13 @@ if (require("data.table")) {
 }
 
 args = (commandArgs(trailingOnly=TRUE))
-key_word = args[1]
+business = args[1]
 
 review_data_match <- fread("review_data_match.csv", encoding = 'UTF-8')
+keywords <- as.vector(t(fread("keyWords", encoding = 'UTF-8', header = F)))
+print(length(keywords))
+review_data_match = review_data_match[review_data_match$business_id == business, ]
 print(nrow(review_data_match))
-all_business_id = unique(review_data_match$business_id)
-print(length(all_business_id))
 
 print("tokenizing")
 #preprocess
@@ -36,13 +37,14 @@ for (i in 1:length(sentences_stemed_tokenized)) {
 
 #sentiment
 print("sentimenting")
-myfunction1 = function(business = "1YC7AbQMlNb5mLUlwwaa3w", keyword = key_word){
+myfunction2 = function(business, keyword){
   #extrect needed info
   f_review_index = (review_data_match$business_id == business)
   f_sentences_stemed = sentences_stemed[f_review_index]
   f_sentences_stemed_tokenized = sentences_stemed_tokenized[f_review_index]
   #match sentence and stars, and do sentiment analysis
   tab = data.frame(business_id = vector(),
+                   Keyword = vector(), 
                    #sentence = vector(), 
                    word_count = vector(), 
                    stars = vector(), 
@@ -51,6 +53,7 @@ myfunction1 = function(business = "1YC7AbQMlNb5mLUlwwaa3w", keyword = key_word){
     for (j in 1:length(f_sentences_stemed[[i]])) {
       if (keyword %in% f_sentences_stemed_tokenized[[i]][[j]]){
         v = data.frame(business_id = business,
+                       Keyword = keyword, 
                        #sentence = f_sentences_stemed[[i]][[j]], 
                        word_count = sentiment(f_sentences_stemed[[i]][[j]])$word_count, 
                        stars = as.numeric(review_data_match$stars[f_review_index][i]), 
@@ -63,13 +66,14 @@ myfunction1 = function(business = "1YC7AbQMlNb5mLUlwwaa3w", keyword = key_word){
 }
 
 output_data = data.frame(business_id = vector(),
-                       #sentence = vector(), 
-                       word_count = vector(), 
-                       stars = vector(), 
-                       sentiment = vector())
-for (i in 1:length(all_business_id)) {
-  output_data = rbind(output_data, myfunction1(business = all_business_id[i], keyword = key_word))
+                         Keyword = vector(), 
+                        #sentence = vector(), 
+                        word_count = vector(), 
+                        stars = vector(), 
+                        sentiment = vector())
+for (i in 1:length(keywords)) {
+  output_data = rbind(output_data, myfunction2(business = business, keyword = keywords[i]))
   print(i)
 }
 
-write.csv(output_data, paste(key_word, ".csv", sep = ""))
+write.csv(output_data, paste(business, ".csv", sep = ""))
