366b412668402e3709d5990da7ec500ddbb6968b
Haohao <haroldsu.us@gmail.com>
2020-12-01 21:06:55 +0800

    Move ShinyApp/ into scripts/

---
+++ scripts/ShinyApp/app.R
@@ -0,0 +1,185 @@
+library(shiny)
+library(dplyr)
+
+# Function to output text
+display_text <- function(Business,Type){
+  # Read score data
+  data <- read.csv("./data/score.csv")
+  
+  # Index of obs whose name or id matching input
+  index_business_match_id <- which(data$business_id == Business)
+  index_business_match_name <- which(data$name == Business)
+  index_business_match <- unique(c(index_business_match_id,index_business_match_name))
+  
+  # Data of the business matching input business name or id
+  data_business_match <- data[index_business_match, ]
+  
+  if(Type != "menu"){
+    # Data grouped by type
+    data_grouped_by_type <- data %>% 
+      group_by(business_id, type) %>% 
+      summarize(counts_of_type = sum(counts), score_of_type = mean(score))
+    
+    if(length(index_business_match) == 0){
+      print(paste('There is neither business id nor business name matching \"', Business, '\".\n', sep = ""))
+    }else if(length(which(data_business_match$type == Type)) == 0){
+      print(paste('There is no review mentioning your', Type, '\".\n', sep = ""))
+    }else{
+      # Score of the matching business with a given type 
+      score <- mean(data_business_match$score[data_business_match$type == Type])
+      
+      # Count of reviews mentioning the given type for the matching business  
+      count_of_reviews <- sum(data_business_match$counts[data_business_match$type == Type])
+      
+      # Percentage of businesses whose score in the given type are lower than the matching business
+      percent_better_than <- round(sum(data_grouped_by_type$score_of_type[data_grouped_by_type$type == Type] < score) 
+                                   / dim(data_grouped_by_type[(data_grouped_by_type$type == Type),])[1] 
+                                   * 100, digits = 1)
+      
+      print(paste('Your', Type, 'score is', round(score,digits = 2), 'given by totally', count_of_reviews,
+                  'reviews mentioning this aspect. It is better than', percent_better_than, 'percent of all',
+                  dim(data_grouped_by_type[(data_grouped_by_type$type == Type),])[1], 'businesses, while the median score is',
+                  round(median(data_grouped_by_type$score_of_type[data_grouped_by_type$type == Type]), digits = 2),
+                  '.\n'))
+    }
+  }else{
+    if(length(which(data_business_match$type == "menu")) == 0){
+      paste("There is no review mentioning any specific food in your menu.")
+    }else{
+      food_list <- data_business_match$key_word[data_business_match$type == "menu"]
+      food_list_impressive <- c()
+      i <- 1
+      food_list_to_improve <- c()
+      j <- 1
+      for(food in food_list){
+        score <- data_business_match$score[data_business_match$key_word == food]
+        score_of_food <- data$score[data$key_word == food]
+        percent_better_than <- round(sum(score_of_food <= score) / length(score_of_food) * 100, digits = 2)
+        if(percent_better_than >= 80){
+          food_list_impressive[i] <- food
+          i <- i + 1
+        } 
+        if(percent_better_than <=20){
+          food_list_to_improve[j] <- food
+          j <- j + 1
+        } 
+      }
+      
+      if(length(food_list_impressive) != 0){
+        output_1 <- 'According to reviews, the following food in your menu are considered to be impressive: \n'
+        for(content in food_list_impressive) output_1 <- paste(output_1, content, sep = " ")
+      }else output_1 <- 'None of food in your menu is considered to be impressive by reviews. '
+
+      if(length(food_list_to_improve) != 0){
+        output_2 <- 'Reviews suggest that the following food in your menu may need to improve: \n'
+        for(content in food_list_to_improve) output_2 <- paste(output_2, content, sep = " ")
+      }else output_2 <- 'Reviews indicate that none of food in your menu mentioned by reviews have to improve.'
+      
+      print(paste(output_1, output_2, sep = "; "))
+
+    }
+  }
+}
+
+# Function to output histogram
+display_hist <- function(Business,Type){
+  if(Type != "menu"){
+    # Read score data
+    data <- read.csv("./data/score.csv")
+    
+    # Index of obs whose name or id matching input
+    index_business_match_id <- which(data$business_id == Business)
+    index_business_match_name <- which(data$name == Business)
+    index_business_match <- unique(c(index_business_match_id,index_business_match_name))
+    
+    # Data of the business matching input business name or id
+    data_business_match <- data[index_business_match, ]
+    
+    # Data grouped by type
+    data_grouped_by_type <- data %>% 
+      group_by(business_id, type) %>% 
+      summarize(counts_of_type = sum(counts), score_of_type = mean(score))
+    
+    if(length(index_business_match) != 0 & length(which(data_business_match$type == Type)) != 0){
+      # Score of the matching business with a given type 
+      score <- mean(data_business_match$score[data_business_match$type == Type])
+      
+      # Percentage of businesses whose score in the given type are lower than the matching business
+      percent_better_than <- round(sum(data_grouped_by_type$score_of_type[data_grouped_by_type$type == Type] < score) 
+                                   / dim(data_grouped_by_type[(data_grouped_by_type$type == Type),])[1] 
+                                   * 100, digits = 1)
+      
+      # Histogram
+      p <- hist(data_grouped_by_type$score_of_type[data_grouped_by_type$type == Type], freq = F,
+                main = Type,
+                cex.axis = 1.2, cex.lab = 1.2, xlab = "score", ylab = NA)
+      abline(v = score, col = "red", lwd = 2)
+      text_position_x_1 <- (min(p$breaks) + score) / 2
+      text_position_x_2 <- (max(p$breaks) + score) / 2
+      text_position_y <- max(p$density) * 0.8
+      text(x = text_position_x_1, y = text_position_y, labels = paste0("|<  ", percent_better_than, "%  >|"),
+           col = "red", cex = 1.2)
+      text(x = text_position_x_2, y = text_position_y, labels = paste0("|<  ", (100 - percent_better_than), "%  >|"),
+           col = "red", cex = 1.2)
+    }
+  }
+}
+
+
+
+# Define UI ====
+ui <- fluidPage(
+  navbarPage(
+    title = "Yelp Data Project",
+    id = "main_navbar",
+    
+    # Tab 1: Attributes ----------
+    tabPanel(
+      title = "Attribute",
+      
+    ), # End of Tab 1
+    
+    # Tab 2: Feedback for each business ----------
+    tabPanel(
+      title = "Feedback",
+      sidebarLayout(
+        # Sidebar panel for inputs ----------
+        sidebarPanel(
+          # Input: character entry for business name or business ID -----
+          textInput(inputId = "Business_input",
+                    label = "Input your business name or ID:",
+                    value = "Po' Boys Restaurant"),
+          # Input: Selector for choosing type to analyze -----
+          selectInput(inputId = "Type_input",
+                      label = "Choose a type:",
+                      choices = c("service", "price", "environment", "food quality", "menu")),
+          submitButton("Submit")
+          
+        ), # End of sidebarPanel
+        
+        # Main panel for displaying outputs
+        mainPanel(
+          # Output: text content
+          h3(htmlOutput("text")),
+          # Output: histogram
+          plotOutput("Hist"),
+          
+        ) # End of mainPanel
+      ) # End of sidebarLayout
+    ) # End of Tab 2
+    
+  ) # End of navbarPage
+  
+) # End of ui
+
+
+server <- function(input, output){
+  output$text <- renderUI({
+    display_text(Business = input$Business_input, Type = input$Type_input)
+  })
+  output$Hist <- renderPlot({
+    display_hist(Business = input$Business_input, Type = input$Type_input)
+  })
+}
+
+shinyApp(ui, server)
